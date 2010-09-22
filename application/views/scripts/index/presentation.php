b4cab952a02ecb47e01e87c40c868a20a256101f
root <root@eviasdev.(none)>
2010-09-22 21:39:18 +0000

    add presentation page

---
+++ application/views/scripts/index/presentation.php
@@ -0,0 +1,48 @@
+<H1>Développement modulaire</h1>
+<p>
+Je vous souhaite la bienvenue sur mon serveur web dédié au développement web et logiciel.
+</p>
+
+<p>
+La plupart des parties de ce site web sont en constante évolution et leur apparence ne
+réprésente donc pas l'apparence finale d'un développement officiel. Des versions stables
+des différents modules seront publiés tout au long des développements.
+</p>
+
+<p>
+La séparation en différents modules permet un emploi plus spécialisés des différentes
+ressources utilisées par une application web. Ainsi il est important de bien réfléchir
+avant même l'implémentation des principes de bases.
+</p>
+
+<p>
+Le modulage peut devenir très complexe quand il s'agit de logiciels de grande envergure
+et il ne devrait jamais trop ralentir le développement d'un logiciel. Il est clair que
+développer un catalogue de produits "from scratch" n'est pas toujours simple, car plusieurs
+aspects de celui-ci peuvent être diviser en plusieurs sous modules. Ainsi on se retrouve
+avec un projet qui est composé d'un nombre plus ou moins important de modules.
+</p>
+
+<p>
+Certain pourrait penser que cette façon de développer peut porter à une maintenance plus couteuse
+et moins effectives. C'est là qu'intervient le point fort des modules, un modules effectue des
+accès aux ressources qui lui sont propres. S'il y a un soucis quelque part, il est facile d'en
+déduire lequel des modules est en faute. Bien que l'architecture d'un projet s'en retrouve un
+rien complexifiée, la maintenance, elle, en devient nettement plus rapide et plus effectives.
+</p>
+
+<p>
+Cette introduction fait effet de présentation de ma manière de travailler. J'exerce l'emploi
+de développeur web depuis le mois de Septembre 2009. Cela dit je développe depuis mes 12
+ans (2000) des projets qui me tiennent à coeur, et que je compte vous présenter par le biais de
+cette plateforme de développement. Mes compétences sont très avancées en C et C++, avancées en PHP et
+en Javascript, tandis qu'elles sont juste suffisantes en Java.
+</p>
+
+<p>
+Merci de votre lecture et je vous souhaite bien du plaisir dans le monde d'eVias.
+</p>
+
+<p>
+Saive, Grégory
+</p>
