08dcd463b5d770358407597c394f4cdbeeb4e711
Grégory Saive <saive.gregory@gmail.com>
2010-11-16 14:31:21 +0000

    add version  file and update to actual version ; add twitter widget css file ; add twitterTest unitary test for later implementation

---
+++ tests/library/twitterTest.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+class twitterTest
+    extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+
+    }
+
+    public function tearDown()
+    {
+
+    }
+
+}
+
